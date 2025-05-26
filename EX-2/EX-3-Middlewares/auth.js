f4c47fe99a8d71444cd1c2dd7b0a4d05e78a6c55
Phal Sovandy <sovandy.phal@student.cadt.edu.kh>
2025-05-26 10:17:16 +0700

    Update auth.js

--- EX-2/EX-3-Middlewares/auth.js
+++ EX-2/EX-3-Middlewares/auth.js
@@ -3,10 +3,10 @@ const TOKEN_KEY = "xyz123";
 export default function tokenValidator(req, res, next) {
   const { token } = req.query;
   if (token == undefined) {
-    res.status(401).send("Missing Token!");
+    return res.status(401).send("Missing Token!");
   }
   if (token !== TOKEN_KEY) {
-    res.status(401).send(`Incorrect token: ${token}`);
+    return res.status(401).send(`Incorrect token: ${token}`);
   }
   next();
 }
