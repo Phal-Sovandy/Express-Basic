fdb691cd506e134aea094da765572f304636951f
Phal Sovandy <sovandy.phal@student.cadt.edu.kh>
2025-05-26 10:13:34 +0700

    Update validateQuery.js

--- EX-2/EX-3-Middlewares/validateQuery.js
+++ EX-2/EX-3-Middlewares/validateQuery.js
@@ -1,18 +1,18 @@
 export default function creditValidator(req, res, next) {
   const { minCredits, maxCredits } = req.query;
   if (minCredits !== undefined && !Number(minCredits)) {
-    return res.status(400).send("Min Credit MUST be number");
+    res.status(400).send("Min Credit MUST be number");
   }
   if (maxCredits !== undefined && !Number(maxCredits)) {
     console.log(maxCredits);
-    return res.status(400).send("Max Credit MUST be number");
+    res.status(400).send("Max Credit MUST be number");
   }
   if (
     minCredits !== undefined &&
     maxCredits !== undefined &&
     minCredits > maxCredits
   ) {
-    return res.status(400).send("Min Credit is greater than Max Credit");
+    res.status(400).send("Min Credit is greater than Max Credit");
   }
   next();
 }
